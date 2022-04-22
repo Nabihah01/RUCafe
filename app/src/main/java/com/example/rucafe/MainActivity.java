82f87080469d2402e4c923eda20fd4f242c460e4
Maryam Syed <49998057+marsyed15@users.noreply.github.com>
2022-04-22 14:34:03 -0400

    img buttons

--- app/src/main/java/com/example/rucafe/MainActivity.java
+++ app/src/main/java/com/example/rucafe/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import java.util.ArrayList;
 
@@ -27,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
 //        yourOrder = new Order(new ArrayList<>(), orderNum);
 //        storeOrders = new StoreOrders();
 
-        Button donutsButton = findViewById(R.id.orderDonuts);
+        ImageButton donutsButton = findViewById(R.id.orderDonuts);
 
         donutsButton.setOnClickListener(new View.OnClickListener() {
             @Override
